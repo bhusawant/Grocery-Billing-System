3d1adbbd4ecc4fdceef952fbb7eb280e484af566
Bhuvan <102148710+bhusawant@users.noreply.github.com>
2022-12-22 02:42:35 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,21 @@
+
+package grocery_billing_system;
+
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+
+Conn() {
+	try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		c = DriverManager.getConnection("jdbc:mysql://localhost:3306/grocerybillingsystem", "root", "1234");
+		s = c.createStatement();
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+    }
+}
+
